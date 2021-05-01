f514d5305cc14344dd746cd3047c8eeb2c92df5c
Ethan136 <ethanliust@gmail.com>
2021-05-01 18:23:57 +0800

    [2] 內容精簡 + 兩個方法的筆記完成

--- Easy/[2] Add Two Numbers/2.add-two-numbers.cpp
+++ Easy/[2] Add Two Numbers/2.add-two-numbers.cpp
@@ -16,22 +16,21 @@
  * };
  */
 
-#define DO_DIGIT_ADD( StoreValue, AddValue, CarryDigit )											\
-					StoreValue += ( AddValue + CarryDigit );										\
-					CarryDigit = 0;																	\
-																									\
-					if( StoreValue >= 10 ) {															\
-						StoreValue -= 10;															\
-						CarryDigit = 1;																\
-					};
+#define DO_DIGIT_ADD( StoreValue, TakeValue, CarryDigit ) \
+	StoreValue += ( TakeValue + CarryDigit );             \
+	CarryDigit = 0;                                       \
+                                                          \
+	if( StoreValue >= 10 ) {                              \
+		StoreValue -= 10;                                 \
+		CarryDigit = 1;                                   \
+	}
 
-#define PREPARE_TO_RETURN
-enum ESCAN_STATE {
-	STATE_SCAN_BOTH_LIST,
-	STATE_SNATCH_REMAINED_VALUE_TAKE_LIST_TO_STORE_LIST,
-	STATE_SCAN_TO_LAST_OF_VALUE_TAKE_LIST,
-	STATE_ONLY_SCAN_STORE_LIST,
-};
+#define DO_DIGIT_ADD_AND_GO_NEXT_NODE( StoreNode, ValTakeNode, CarryDigit ) \
+	DO_DIGIT_ADD( StoreNode->val, ValTakeNode->val, CarryDigit )            \
+                                                                            \
+                                                                            \
+	StoreNode = StoreNode->next;                                            \
+	ValTakeNode = ValTakeNode->next;
 
 class Solution {
 public:
@@ -40,116 +39,52 @@ public:
 		// about 9 + 8 = 17 -> carry digit = 1
 		int nCarryDigit = 0;
 
-		// set one list to store and the other to take value
-		ListNode *pStoreNode = l1;
-		ListNode *pValueTakeNode = l2;
-
-		// the spared node in case there's any need to expand the list
-		ListNode *pSpareNode;
-
 		// the resultant list
-		ListNode *pResult = pStoreNode;
+		ListNode *pResult = l1;
 
-		// do scanning
-		int nScanState = STATE_SCAN_BOTH_LIST;
+		// make fake head
+		ListNode* pPrevl1;
+		ListNode *pSpareNode = l2;
+
+		// Step1: scan both list
 		while( true ) {
 
-			switch( nScanState ) {
-			case STATE_SCAN_BOTH_LIST:
+			// do backup before add
+			pPrevl1 = l1;
 
-				// based on the constrains of the problem: the least length of l1 and l2 should be 1
-				// os there's no need to prevent the condition that l1 == null or l2 == null
+			// do add and go to next node
+			DO_DIGIT_ADD_AND_GO_NEXT_NODE( l1, l2, nCarryDigit );
 
-				// if the store list comes to the end
-				if( pStoreNode->next == nullptr ) {
-					nScanState = STATE_SNATCH_REMAINED_VALUE_TAKE_LIST_TO_STORE_LIST;
-					break;
-				}
-
-				// if the take value list comes to the end
-				if( pValueTakeNode->next == nullptr ) {
-					nScanState = STATE_SCAN_TO_LAST_OF_VALUE_TAKE_LIST;
-					break;
-				}
-
-				// add value
-				DO_DIGIT_ADD( pStoreNode->val, pValueTakeNode->val, nCarryDigit );
-
-				// advance to next node
-				pStoreNode = pStoreNode->next;
-				pValueTakeNode = pValueTakeNode->next;
+			// if l1 or l2 is empty
+			if( ( l1 == nullptr ) || ( l2 == nullptr ) ) {
 				break;
-
-			case STATE_SNATCH_REMAINED_VALUE_TAKE_LIST_TO_STORE_LIST:
-
-				// link to the other list
-				pStoreNode->next = pValueTakeNode->next;
-
-				// set the current node of the other list as "last node"
-				pValueTakeNode->next = nullptr;
-
-				// advance to single list scan operation
-				nScanState = STATE_SCAN_TO_LAST_OF_VALUE_TAKE_LIST;
-				break;
-
-			case STATE_SCAN_TO_LAST_OF_VALUE_TAKE_LIST:
-
-				// add value
-				DO_DIGIT_ADD( pStoreNode->val, pValueTakeNode->val, nCarryDigit );
-
-				// spare the last value take node in case there's any need for augmentation
-				pSpareNode = pValueTakeNode;
-				pSpareNode->val = 0;
-
-				// if the next store node is empty
-				if( pStoreNode->next == nullptr ) {
-
-					// if carry digit exist
-					if( nCarryDigit ) {
-						pSpareNode->val = nCarryDigit;
-						pStoreNode->next = pSpareNode;
-					}
-
-					// return result
-					return pResult;
-				}
-
-				// advance to next of store node
-				pStoreNode = pStoreNode->next;
-
-				// advance to single list scan operation
-				nScanState = STATE_ONLY_SCAN_STORE_LIST;
-				break;
-
-			case STATE_ONLY_SCAN_STORE_LIST:
-
-				// if there's no carry digit
-				if( nCarryDigit == 0 ) {
-					return pResult;
-				}
-
-				// add value
-				DO_DIGIT_ADD( pStoreNode->val, 0, nCarryDigit );
-
-				// if the next store node is empty
-				if( pStoreNode->next == nullptr ) {
-
-					// if carry digit exist
-					if( nCarryDigit ) {
-						pSpareNode->val = nCarryDigit;
-						pStoreNode->next = pSpareNode;
-					}
-
-					// return result
-					return pResult;
-				}
-
-				// go to next node
-				pStoreNode = pStoreNode->next;
-				break;
-
 			}
 		}
+
+		// check whether l1 is at the end, snatch remained nodes of l2
+		if( l1 == nullptr ) {
+			pPrevl1->next = l2;
+			l1 = l2;
+		}
+
+		// Step2: scan only l1
+		while( l1 != nullptr ) {
+
+			DO_DIGIT_ADD( l1->val, 0, nCarryDigit );
+
+			pPrevl1 = l1;
+			l1 = l1->next;
+		}
+
+		// if carry digit exist, augment l1
+		if( nCarryDigit ) {
+			pPrevl1->next = pSpareNode;
+			pPrevl1->next->val = 1;
+			pPrevl1->next->next = nullptr;
+		}
+
+		// return result
+		return pResult;
     }
 };
 // @lc code=end
